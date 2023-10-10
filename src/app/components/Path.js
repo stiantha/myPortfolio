b61712bda917f4a499b7582d39e7f0baa7ca07dd
stiantha <stian1301@gmail.com>
2023-10-10 12:44:29 +0200

    swe

--- src/app/components/Path.js
+++
@@ -1,8 +0,0 @@
-export default ({ description }) => {
-    return /*html*/`
-        <div class="path">
-            <h1 class="h1 path__name">${window.location.pathname.slice(1)}</h1>
-            <p class="path__description">${description}</p>
-        </div>
-    `
-}
