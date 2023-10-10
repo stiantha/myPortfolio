5070978956a52311efd87dcac6b0023e76b45090
stiantha <stian1301@gmail.com>
2023-10-10 16:17:05 +0200

    dssd

---
+++ portfolio-main/src/app/consts/routes.js
@@ -0,0 +1,27 @@
+
+/**
+ * @type {import("../../types/Routes").Routes}
+ */
+export default {
+    "/": {
+        name: "home",
+        element: "Home",
+    },
+    "/projects": {
+        name: "projects",
+        element: "Projects",
+    },
+    "/about-me": {
+        name: "about",
+        element: "About",
+    },
+    "/404": {
+        name: "404",
+        element: "PageNotFound",
+    },
+    
+    // "/contacts": {
+    //     name: "contacts",
+    //     element: "Contacts",
+    // },
+};
