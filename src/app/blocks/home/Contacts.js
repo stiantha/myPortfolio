e2e9bcd84e1d7cab377216352a44cf76e36d38f7
Stian Tharaldsen <stiantharaldsen@hotmail.com>
2024-01-26 11:55:36 +0100

    update 39 files and delete 2 files

--- src/app/blocks/home/Contacts.js
+++ src/app/blocks/home/Contacts.js
@@ -17,6 +17,7 @@ export default (t) => {
             <h2 class="h2">${t.title}</h2>
             <div class="contacts__content">
                 <p class="contacts__description">${t.text}</p>
+                <img class="contacts__image" src="/images/signature.png" alt="">
                 <div class="contacts__media">
                     <h3 class="contacts__title">${t.media}</h3>
                     <div class="contacts__list">
--- src/app/components/Dots.js
+++ src/app/components/Dots.js
@@ -1,22 +1,30 @@
 const radius = 4;
 const gap = 16;
 
-export default ({ width = 5, height = 5 } = {}) => {
-    const svgWidth = (radius * 2 + gap) * width - gap
-    const svgHeight = (radius * 2 + gap) * height - gap
+export default ({ width = 7, height = 7 } = {}) => {
+    const svgWidth = (radius * 2 + gap) * width - gap;
+    const svgHeight = (radius * 2 + gap) * height - gap;
+
+    // Define the positions of the stars in the Orion constellation
+    const stars = [
+        { x: 4, y: 1 },
+        { x: 6, y: 2 },
+        { x: 3, y: 3 },
+        { x: 6, y: 4 },
+        { x: 3, y: 5 },
+        { x: 9, y: 6 },
+        { x: 1, y: 7 },
+    ];
 
     return /*html*/ `
         <svg width="100%" class="dots" viewBox="0 0 ${svgWidth} ${svgHeight}">
-            ${new Array(width)
-                .fill(new Array(height).fill(""))
-                .map((arr, i) =>
-                    arr.map((_, j) => {
-                        const x = radius + i * (radius * 2 + gap);
-                        const y = radius + j * (radius * 2 + gap);
+            ${stars
+                .map(star => {
+                    const x = radius + star.x * (radius * 2 + gap);
+                    const y = radius + star.y * (radius * 2 + gap);
 
-                        return /*html*/ `<circle cx="${x}" cy="${y}" r="${radius}" />`;
-                    }).join("")
-                )
+                    return /*html*/ `<circle cx="${x}" cy="${y}" r="${radius}" />`;
+                })
                 .join("")}
         </svg>
     `;
