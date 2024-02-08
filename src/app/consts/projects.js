7c0c9a32f2eff67ebfdf7fde795dd5a0297e7be7
Stian Tharaldsen <stiantharaldsen@hotmail.com>
2024-02-08 06:35:53 +0100

    update 32 files and delete 2 files

--- src/app/consts/projects.js
+++ src/app/consts/projects.js
@@ -6,7 +6,6 @@ const projects = [
         id: "discord-timer-bot",
         links: {
             github: "/SoD-ResetTimer/",
-            live: "discord.com/oauth2/authorize?client_id=1198244865465667634&permissions=67108864&scope=bot"
         },
         techs: ["python", "paas"],
         hasImage: true,
